147460099d189ecd9d08d0c33a58561eb9e423af
Vinicius Roveran <viniroveran@gmail.com>
2024-09-01 22:27:03 -0300

    feat: create user on authentication

--- frontend/environment.d.ts
+++ frontend/environment.d.ts
@@ -4,6 +4,7 @@ declare global {
             AUTH_SECRET: string,
             AUTH_GOOGLE_ID: string,
             AUTH_GOOGLE_SECRET: string,
+            NEXT_PUBLIC_BACKEND_URL: string,
         }
     }
 }
--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,20 +1,23 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import * as process from "node:process";
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
   callbacks: {
-    async signIn({ account, profile }) {
-      // TODO: call backend API to create user
+    async signIn({account, profile}) {
+      fetch(process.env.NEXT_PUBLIC_BACKEND_URL + '/api/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: profile?.name,
+          email: profile?.email,
+          avatar: profile?.picture
+        })
+      })
       return true
     },
-    async session({ session, token }) {
-      console.log(session)
-      console.log(token)
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
-
-      return session
-    }
   },
   providers: [
     Google({
