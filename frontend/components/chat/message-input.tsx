c3f844f1d0a732bfc927b4af16d5ef2d849e0727
Vinicius Roveran <viniroveran@gmail.com>
2024-09-01 22:27:03 -0300

    feat: press enter to send message

--- frontend/components/chat/message-input.tsx
+++ frontend/components/chat/message-input.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import {useState} from "react";
+import {useEffect, useState, useCallback} from "react";
 import {useSocket} from "@context/socket-context";
 import {User} from "@lib/definitions";
 
 export default function MessageInput(user: User) {
   const {sendMessage} = useSocket();
   const [message, setMessage] = useState("");
-  const handleSendMessage = () => {
+  const handleSendMessage = useCallback(() => {
     sendMessage(message, user.email, user.name, user.image);
-  };
+    setMessage("");
+  }, [message, sendMessage, user.email, user.image, user.name]);
+
+  useEffect(() => {
+    const listener = (event: { code: string; }) => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        console.log("Enter key was pressed. Run your function.");
+        // event.preventDefault();
+        handleSendMessage();
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, [handleSendMessage]);
 
   return (
     <div className="bg-base-300 p-4 flex items-center gap-2">
       <input
         onChange={(e) => setMessage(e.target.value)}
+        value={message}
         className="flex-1 px-4 py-2 input input-bordered input-primary"
         placeholder="Message..."
       />
