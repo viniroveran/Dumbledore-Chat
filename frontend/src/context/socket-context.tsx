bfab3da9ecf99b7a3d56cddb090afb6343afbe39
Vinicius Roveran <viniroveran@gmail.com>
2024-09-01 22:27:03 -0300

    chore: remove TODO

--- frontend/src/context/socket-context.tsx
+++ frontend/src/context/socket-context.tsx
@@ -44,16 +44,6 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
     setSocket(_socket);
 
     // TODO: get messages from backend
-    const previousMessages: ISocketReceivedMessage[] = [
-      {
-        message: "Oi, eu sou um gatinho!",
-        message_id: "001",
-        user_email: "gatinho@google.com",
-        user_name: "Gatinho",
-        user_image: "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
-      }
-    ]
-    setMessages(previousMessages)
 
     return () => {
       _socket.off("message", onMessageReceived);
